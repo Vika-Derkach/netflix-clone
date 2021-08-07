2174e2c423ed87188411847a3363f5e2dc108e41
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-07 21:55:54 +0300

    accordion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaqsContainer } from "./containers/faqs";
 import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <>
       {" "}
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />{" "}
     </>
   );
