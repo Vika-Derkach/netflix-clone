7430943bebfe35abaa92e087b3c524708b440225
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-11 12:49:23 +0300

    Pages: Home, Browse, SignIn, SignUp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import { Home } from "./pages";
+import { Browse, Home, Signin, Signup } from "./pages";
 export default function App() {
   return (
     <Router>
       <Route exact path={ROUTES.HOME}>
         <Home />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
       </Route>
     </Router>
   );
