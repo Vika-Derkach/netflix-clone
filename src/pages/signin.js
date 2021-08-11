646bd3f65a7fbcbf07f9296511f6ca4ecdcd9641
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-11 19:27:15 +0300

    Firebase top level authentication

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
+import { FirebaseContext } from "../context/firebase";
 export default function Signin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const isInvalid = password === "" || emailAddress === "";
   const handleSignIn = (event) => {
     event.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
   };
   return (
     <>
