2ee390a7694e8ea8723c2fd32cc7649da82af3c0
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-11 15:52:48 +0300

    firebase

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import "normalize.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./app";
+import { FirebaseContext } from "./context/firebase";
 import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
 render(
   <>
-    {" "}
-    <GlobalStyles />
-    <App />{" "}
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />{" "}
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,32 @@
+import Firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const config = {
+  // apiKey: '',
+  // authDomain: '',
+  databaseURL: "https://netflix-course.firebaseio.com",
+  // projectId: '',
+  // storageBucket: '',
+  // messagingSenderId: '',
+  // appId: '',
+  apiKey: "AIzaSyDfX4K6cemFhGw6anjQVeXT3m_aNAv782o",
+  authDomain: "netflix-course.firebaseapp.com",
+  projectId: "netflix-course",
+  storageBucket: "netflix-course.appspot.com",
+  messagingSenderId: "531324622403",
+  appId: "1:531324622403:web:16fecf602690d0143426f6",
+  //   apiKey: "AIzaSyDfX4K6cemFhGw6anjQVeXT3m_aNAv782o",
+  //   authDomain: "netflix-course.firebaseapp.com",
+  //   projectId: "netflix-course",
+  //   storageBucket: "netflix-course.appspot.com",
+  //   messagingSenderId: "531324622403",
+  //   appId: "1:531324622403:web:b952c1160ff894273426f6",
+};
+
+const firebase = Firebase.initializeApp(config);
+// 2) when seeding the database you'll have to uncomment this!
+//seedDatabase(firebase);
+// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
+
+export { firebase };
