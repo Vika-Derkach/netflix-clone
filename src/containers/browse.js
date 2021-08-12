84a2766a4bf78b66872f926c048cfb316a869ad3
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-12 20:54:19 +0300

    Profile component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { FirebaseContext } from "../context/firebase";
+import { SelectProfileContainer } from "./profiles";
 export function BrowseContainer({ slides }) {
-  return <p>Hello!!</p>;
+  const [profile, setProfile] = useState({});
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+  return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
