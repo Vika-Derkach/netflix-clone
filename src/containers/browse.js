e771ad8c104d17fb435a8f5a100ca14ef876cabf
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-08-12 21:24:18 +0300

    Loading profile component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,9 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Header, Loading } from "../components";
 import { FirebaseContext } from "../context/firebase";
 import { SelectProfileContainer } from "./profiles";
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
   const { firebase } = useContext(FirebaseContext);
+
   const user = firebase.auth().currentUser || {};
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1">
+        <p>hello</p>
+      </Header>
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -16,12 +16,12 @@ export function SelectProfileContainer({ user, setProfile }) {
         <Profiles.Title>Who's watching?</Profiles.Title>
         <Profiles.List>
           <Profiles.User
-            // onClick={() =>
-            // //   setProfile({
-            // //     displayName: user.displayName,
-            // //     photoURL: user.photoURL,
-            // //   })
-            // }
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
             data-testid="user-profile"
           >
             <Profiles.Picture src={user.photoURL} />
