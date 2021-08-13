52199068602658d73de907ab5b2941e9edaef9fc
darkander <kingdarkander@gmail.com>
2021-08-13 18:00:04 +0300

    Player component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Card, Header, Loading } from "../components";
+import { Card, Header, Loading, Player } from "../components";
 import * as ROUTES from "../constants/routes";
 import { FirebaseContext } from "../context/firebase";
 import logo from "../logo.svg";
@@ -99,11 +99,10 @@ export function BrowseContainer({ slides }) {
             </Card.Entities>
 
             <Card.Feature category={category}>
-              {/* <Player>
+              <Player>
                 <Player.Button />
                 <Player.Video src="/videos/bunny.mp4" />
-              </Player> */}
-              <p>Hello</p>
+              </Player>
             </Card.Feature>
           </Card>
         ))}
